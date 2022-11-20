b96a2ff7d4b18a593d8393e310911260e058150a
brickwall <sk292207@gmail.com>
2022-11-20 21:12:20 +0530

    Initial prototype upload

---
+++ spec/test_spec.rb
@@ -0,0 +1,37 @@
+class Wallet
+    attr_accessor :worth
+    def initialize(money)
+        @worth = money
+    end
+
+end
+
+#local let
+describe Wallet do 
+    context 'let vs before' do
+        let(:wallet){ Wallet.new(2000) }
+        it 'evalutes the worth' do 
+            expect(wallet.worth).to eq 2000
+        end
+    end
+end
+
+# #global let
+# describe Wallet do 
+#     let(:wallet){ Wallet.new(2000) }
+#     context 'let vs before' do
+#         it 'evalutes the worth' do 
+#             expect(wallet.worth).to eq 2000
+#         end
+#     end
+# end
+
+
+# describe Wallet do 
+#     before{ $wallet = Wallet.new(2000) }
+#     context 'let vs before' do
+#         it 'evalutes the worth' do 
+#             expect($wallet.worth).to eq 2000
+#         end
+#     end
+# end
